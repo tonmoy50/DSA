baa4852a09fd2ec0d2bfe08361b132fde6255626
tonmoy <tonmoyhalder50@gmail.com>
2025-11-24 22:01:34 -0500

    practice update

---
+++ src/practice/sorting_algo.py
@@ -0,0 +1,45 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    sorted_left = merge_sort(left_half)
+    sorted_right = merge_sort(right_half)
+
+    return merge(sorted_left, sorted_right)
+
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+
+    return result
+
+
+def main():
+    my_list = [38, 27, 43, 3, 9, 82, 10]
+    sorted_list = merge_sort(my_list)
+    print(sorted_list)
+
+
+if __name__ == "__main__":
+    main()
